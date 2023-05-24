6423c641f0a6807b8208c118810719e0c32936b7
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-24 15:30:24 +0200

    add: push

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import AppMain from './components/AppMain.vue'
+import AppHeader from './components/AppHeader.vue'
+import Searchbar from './components/Searchbar.vue'
+import store from './store'
+import SeachbarVue from './components/Seachbar.vue'
+
+
+
+export default{
+
+  data() {
+    return {
+        store,
+    }
+  },
+
+  components:{
+    AppHeader,
+    AppMain,
+    Searchbar,
+  }
+}
+</script>
+
+<template>
+<AppHeader /> 
+<AppMain /> 
+</template>
+
+<style>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+import SearchBar from './Searchbar.vue'
+
+export default{
+    components: {
+        SearchBar,
+    }
+}
+</script>
+
+<template>
+    <SeachBar/>
+</template>
+
+<style lang="scss" scoped>
+</style>
