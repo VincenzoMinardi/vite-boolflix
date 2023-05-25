1e4875fd10239d79c8b24a5d153e9f01eb9164b4
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-25 13:13:04 +0200

    add: movies

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,49 @@
 <script>
+import { store } from '../store'
+import AppMovies from './AppMovies.vue'
+import AppSeries from './AppSeries.vue'
 
 
 export default{
+    data() {
+        return {
+            store,
+        }
+    },
+
+    components:{
+        AppMovies,
+        AppSeries
+    }
 }
 </script>
 
 <template>
-    
+    <div class="container">
+        <div class="movies">
+            <AppMovies v-for="movie in ArrMovies"
+            :key="movie.id"
+            :title="movie.title"
+           :original_title="movie.original_title"
+            :original_language="movie.original_language"
+            :vote_average="movie.vote_average"
+            />
+        </div>
+        <div class="series">
+            <AppSeries v-for="series in ArrSeries" 
+            :key="series.id"
+            :name="series.name"
+            :original_name="series.original_name"
+            :original_language="series.original_language"
+            :vote_average="series.vote_average"
+            />
+        </div>
+    </div>
 </template>
 
 
 <style lang="scss" scoped>
+.container{
+    height: 100vh;
+}
 </style>
---
+++ src/components/AppMovies.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: {
+    title: String,
+    original_title: String,
+    original_language: String,
+    vote_average: Number,
+  },
+};
+</script>
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <h2>{{ origina_title }}</h2>
+        <h3>{{ original_language }}</h3>
+        <div>{{ vote_average }}</div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+</style>
